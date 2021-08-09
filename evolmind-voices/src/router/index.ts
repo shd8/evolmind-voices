d78eaf71f06d41ec6561de938b05b350d486969b
Fernando Gomez <shadyfast8.services@gmail.com>
2021-08-09 12:43:34 +0200

    Test App index

--- evolmind-voices/src/router/index.ts
+++ evolmind-voices/src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 import Home from '../views/Home.vue';
+import Favorites from '../views/Favorites.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -13,7 +14,7 @@ const routes: Array<RouteRecordRaw> = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Favorites.vue'),
+    component: Favorites,
   },
 ];
 
